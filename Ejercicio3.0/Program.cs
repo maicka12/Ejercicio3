363782fdd803c9c3f5206a6f2a4778a58f477a4e
maicka <maicka1997@gmail.com>
2022-07-05 03:27:05 +0200

    Agregar archivos de proyecto.

---
+++ Ejercicio3.0/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio3
+{
+    internal class Class1
+    {
+        static void Main()
+        {
+
+            int X = 17;
+            int Y = 22;
+            double N = 30.23;
+            double M = 40.46;
+
+            double A = Convert.ToDouble(X);
+            double B = Convert.ToDouble(Y);
+
+            Console.WriteLine(X + Y);
+            Console.WriteLine(X - Y);
+            Console.WriteLine(X * Y);
+            Console.WriteLine(X / Y);
+            Console.WriteLine(X % Y);
+            Console.WriteLine(N + M);
+            Console.WriteLine(N - M);
+            Console.WriteLine(N * M);
+            Console.WriteLine(N / M);
+            Console.WriteLine(N % M);
+            Console.WriteLine(A);
+            Console.WriteLine(B);
+            Console.WriteLine(A + B + X + Y);
+            Console.WriteLine(A * B * X * Y);
+
+
+
+        }
+
+    }
+}
